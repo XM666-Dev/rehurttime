af66d06fc73f12d78495f5a7666a827f4b9b7f1f
XM666-Dev <x94732568960@163.com>
2026-06-20 13:00:31 +0800

    Fix

--- src/main/java/com/xm666/rehurttime/handler/ExpressionHandler.java
+++ src/main/java/com/xm666/rehurttime/handler/ExpressionHandler.java
@@ -41,7 +41,6 @@ public class ExpressionHandler {
         return Set.of();
     }
 
-    @SuppressWarnings({"OptionalGetWithoutIsPresent", "ConstantValue"})
     public static String getHolderType(Holder<?> holder) {
         if (holder != null) {
             var key = holder.unwrapKey().get();
