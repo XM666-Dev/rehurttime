e4480ae8c8cf787802338fa38786326daa380a2c
XM666-Dev <x94732568960@163.com>
2026-06-20 12:41:17 +0800

    Fix

--- src/main/java/com/xm666/rehurttime/mixin/AppliesKnockbackMixin.java
+++ src/main/java/com/xm666/rehurttime/mixin/AppliesKnockbackMixin.java
@@ -1,5 +1,6 @@
 package com.xm666.rehurttime.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import com.xm666.rehurttime.Config;
 import com.xm666.rehurttime.handler.ExpressionHandler;
 import net.minecraft.world.damagesource.DamageSource;
@@ -11,8 +12,8 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 public class AppliesKnockbackMixin {
     @Mixin(LivingEntity.class)
     private static class LivingEntityMixin {
-        @ModifyVariable(method = "hurt", at = @At(value = "STORE", ordinal = 1), name = "flag1", ordinal = 1)
-        private boolean modifyKnockback(boolean knockback, DamageSource source, float amount) {
+        @ModifyVariable(method = "hurtServer", at = @At(value = "STORE", ordinal = 1), name = "flag1", ordinal = 1)
+        private boolean modifyKnockback(boolean knockback, @Local(argsOnly = true) DamageSource source) {
             if (knockback) return true;
 
             var appliesKnockbackPredicate = Config.APPLIES_KNOCKBACK_PREDICATE.get();
