605b02158a29394327a06eef50d05a39a67ecca3
XM666-Dev <x94732568960@163.com>
2026-06-20 12:35:06 +0800

    Fix

--- src/main/java/com/xm666/rehurttime/ReHurtTime.java
+++ src/main/java/com/xm666/rehurttime/ReHurtTime.java
@@ -1,17 +1,11 @@
 package com.xm666.rehurttime;
 
 import com.mojang.logging.LogUtils;
-import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
 import org.slf4j.Logger;
 
 @Mod(ReHurtTime.MODID)
 public class ReHurtTime {
     public static final String MODID = "rehurttime";
     public static final Logger LOGGER = LogUtils.getLogger();
-
-    public ReHurtTime(ModContainer modContainer) {
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-    }
 }
