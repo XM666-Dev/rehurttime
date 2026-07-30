33f41f2495552c9da777e8f94a70c31b265bcf13
XM666-Dev <x94732568960@163.com>
2026-07-30 16:21:03 +0800

    Fix mod constructor

--- src/main/java/com/xm666/rehurttime/ReHurtTime.java
+++ src/main/java/com/xm666/rehurttime/ReHurtTime.java
@@ -2,6 +2,8 @@ package com.xm666.rehurttime;
 
 import com.mojang.logging.LogUtils;
 import com.xm666.rehurttime.handler.LogHandler;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
@@ -12,8 +14,10 @@ public class ReHurtTime {
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public ReHurtTime(FMLJavaModLoadingContext context) {
-        var container = context.getContainer();
-        var eventBus = context.getModEventBus();
+        this(context.getContainer(), context.getModEventBus());
+    }
+
+    public ReHurtTime(ModContainer container, IEventBus eventBus) {
         Config.init(container);
         LogHandler.LogHandlerConfig.init(eventBus);
     }
